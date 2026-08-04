45f2fe2bbf18f42d1d3bd8464aa5557955e7b7d7
Sanghwa-Na <bdn980@gmail.com>
2026-08-04 19:15:17 +0900

    quiz 클래스 작성

---
+++ 1주차/QuizGame/Quiz.py
@@ -0,0 +1,16 @@
+class Quiz:
+    def __init__(self, question, choices, answer):
+        self.question = question
+        self.choices = choices
+        self.answer = answer # 1 ~ 4
+
+    def show(self): # 문제와 선택지 출력
+        print(f"\n{self.question}")
+
+        for i, choice in enumerate(self.choices, start=1):
+            print(" "*5 + f"{i}, {choice}")
+
+    def check(self,user_answer): # 정답 확인
+        return user_answer == self.answer
+
+    
